021407a14e65f15898fba38cbf288ea7bf0b0814
Alejandro Díaz <alejandrodiazpugh@gmail.com>
2022-11-30 11:24:37 -0600

    Simplificacion de directorio, y trabajo en el frontend

---
+++ src/routes/view.routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Contenedor from '../utils/Contenedor.js';
+
+const products = new Contenedor('./src/data/products.json');
+const productsToDisplay = await products.getAll();
+
+const viewsRouter = express.Router();
+
+viewsRouter.get('/', async (req, res) => {
+	res.render('index', { products: productsToDisplay });
+});
+
+viewsRouter.get('/carrito', async (req, res) => {
+	res.render('carrito', {});
+});
+
+export default viewsRouter;
